370082375629d3f1ee27c302372990b68563de95
Tyler Flint <tylerflint@gmail.com>
2018-11-01 09:31:53 -0600

    Disable russian encoding

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,15 +120,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
-
-TIME_ZONE = 'Europe/Moscow'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+# LANGUAGE_CODE = 'ru-ru'
+# 
+# TIME_ZONE = 'Europe/Moscow'
+# 
+# USE_I18N = True
+# 
+# USE_L10N = True
+# 
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
